7751f1ace5bb1a86be45c4f6a69e374122d70168
drkchiloll <samwomack_@me.com>
2016-05-17 13:29:33 -0500

    import AppComponent beside writing it here

--- app/main.ts
+++ app/main.ts
@@ -1,19 +1,5 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
-import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app',
-  template: (
-    `<h1>Hello {{ name }}!</h1>`
-  )
-})
-
-class AppComponent {
-  name: string;
-
-  constructor() {
-    this.name = 'Angular 2';
-  }
-}
+import { AppComponent } from './app.component';
 
 bootstrap(AppComponent);
