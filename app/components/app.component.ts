02250315848bf709b89513ca74f59dae31684cfe
drkchiloll <samwomack_@me.com>
2016-05-19 16:26:09 -0500

    1.) Created a button tag in our markup that shows the user where to click 2.) Created a fn addArticle that defines what we want to do when the btn is clicked 3.) Added (click) attr on the btn to call addArt fn when btn is clicked 4.) Added attr's #newtitle/#newlink to the input tags (they are local vars to the view)

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -9,12 +9,17 @@ import { Component } from '@angular/core';
 
         <div class='field'>
           <label for='title'>Title:</label>
-          <input name='title' />
+          <input name='title' #newtitle />
         </div>
         <div class='field'>
           <label for='link'>Link:</label>
-          <input name='link' />
+          <input name='link' #newlink />
         </div>
+
+        <button (click)='addArticle(newtitle, newlink)'
+                class='ui positive right floated button'>
+          Submit link
+        </button>
 
       </form>
     `
@@ -23,4 +28,8 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   constructor() {}
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): void {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+  }
 }
