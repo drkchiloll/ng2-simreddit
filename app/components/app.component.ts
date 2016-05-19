5e46df7c87eeee5d79b8f76384df90371695d3cc
drkchiloll <samwomack_@me.com>
2016-05-19 16:11:50 -0500

    Added Add Link Form

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -2,7 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app',
-  templateUrl: 'app/components/app.component.html'
+  template: (
+    `
+      <form class='ui large form segment'>
+        <h3 class='ui header'> Add Link </h3>
+
+        <div class='field'>
+          <label for='title'>Title:</label>
+          <input name='title' />
+        </div>
+        <div class='field'>
+          <label for='link'>Link:</label>
+          <input name='link' />
+        </div>
+
+      </form>
+    `
+  )
 })
 
-export class AppComponent {}
+export class AppComponent {
+  constructor() {}
+}
