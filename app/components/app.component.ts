7fe171d0307ee88fa93e8ffbcd277df9c64b8e34
drkchiloll <samwomack_@me.com>
2016-05-20 07:02:40 -0500

    added ability to add articles to the view using addArticle (previously it was just a console log)

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -120,5 +120,8 @@ export class AppComponent {
 
   addArticle(title: HTMLInputElement, link: HTMLInputElement): void {
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
   }
 }
